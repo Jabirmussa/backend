d242083cbaca9783d06319b59df2f46ecc403ce0
Growskills <jabirmussa185@gmail.com>
2025-04-28 09:21:09 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
-import job from '../lib/corn.js';
+import job from './lib/corn.js';
 
 import authRoutes from './routes/authRoutes.js';
 import bookRoutes from './routes/bookRoutes.js';
-import { connectDB } from '../lib/db.js';
+import { connectDB } from './lib/db.js';
 
 const app = express();
 const PORT = process.env.PORT;
 
-job.start();
+// job.start();
 app.use(express.json()); // Middleware to parse JSON data
 
 app.use("/api/auth", authRoutes);
