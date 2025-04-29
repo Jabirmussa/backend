c9b8d3485fb902c63e4fd3e80a23511286f57978
Growskills <jabirmussa185@gmail.com>
2025-04-29 10:01:40 +0200

    fix: correct route registration for bookRoutes

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT;
 app.use(express.json()); // Middleware to parse JSON data
 
 app.use("/api/auth", authRoutes);
-app.use("/api/auth", bookRoutes);
+app.use("/api/books", bookRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
